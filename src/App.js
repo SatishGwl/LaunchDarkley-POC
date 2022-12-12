da41c490eec2e102af6a44f49ae4cdfe1e026fa0
satishsharma <satishs@dewsolutions.in>
2022-12-12 11:35:20 +0530

    POC-LanchDarkly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { useFlags, withLDProvider } from 'launchdarkly-react-client-sdk';
+import { withLDProvider } from 'launchdarkly-react-client-sdk';
 import HelloWord from './HelloWord'; //We will add this code in the next step
 
 function App() {
-  const userDomain = userEmail.split('@').pop(),  
-  const value = configCatClient.getValue(key, defaultValue, callback, userObject);
-
 
   return (
     <div className="App">
@@ -17,19 +14,12 @@ function App() {
 }
 // Update the export default to use your environment-specific client ID and a sample user:
 export default withLDProvider({
-  const value = configCatClient.getValue(key, defaultValue, callback, countryObject);
-
   clientSideID: '6391d3615a5f060df9a4a991',
-  countryObject:{
-    country: 'india'
-  },
+
   user: {
       "key": "example_user",
       "name": "Example user",
       "email": "User@example.com",
-      custom: {country: countryObject},
-      email: userEmail,
-      country: country,
 
      }
 })(App)
